6cb4fa99698490999c0c53a9015c8afc8ab47e54
Reimar Döffinger <reimar@hokum.(none)>
2007-07-12 23:52:29 +0200

    very different approach allowing to use BlockRAM (still broken though)

--- accumulator.vhdl
+++ accumulator.vhdl
@@ -8,7 +8,7 @@ package accumulator_types is
   constant NUMBLOCKS : integer := 20;
   subtype addblock is std_logic_vector(2*BLOCKSIZE-1 downto 0);
   subtype subblock is std_logic_vector(BLOCKSIZE-1 downto 0);
-  type accutype is array (NUMBLOCKS-1 downto 0) of subblock;
+  type accutype is array (NUMBLOCKS/2-1 downto 0) of subblock;
   subtype flagtype is std_logic_vector(NUMBLOCKS-1 downto 0);
   subtype position is natural range 0 to NUMBLOCKS-2;
   type operation is (op_nop, op_add, op_output);
@@ -43,10 +43,20 @@ entity accumulator is
 end accumulator;
 
 architecture behaviour of accumulator is
-  signal accu : accutype;
+  constant allone : subblock := (others => '1');
+  constant allzero : subblock := (others => '0');
+  signal accu0 : accutype;
+  signal accu1 : accutype;
   signal allmask : flagtype;
   signal allvalue : flagtype;
+  signal input : addblock;
   signal output : addblock;
+  signal sig_op : operation;
+  signal cycle : std_logic;
+  signal carry : std_logic;
+  signal addpos0 : natural;
+  signal addpos1 : natural;
+  signal swap : boolean;
 begin
   process(read)
   begin
@@ -59,79 +69,108 @@ begin
 
   process(clock,reset)
     variable outbuf : addblock;
-    procedure get_accu(p : in position; v : out subblock) is
-    begin
-      if allmask(p) = '1' then
-        v := (others => allvalue(p));
-      else
-        v := accu(p);
-      end if;
-    end get_accu;
+    variable curval : addblock;
+    variable curval0 : subblock;
+    variable curval1 : subblock;
+	 variable addpos : natural;
 
-    procedure set_accu(p : in position; v : in subblock) is
-      variable replicate : subblock := (others => v(0));
-    begin
-      if v = replicate then
-        allmask(p) <= '1';
-      else
-        allmask(p) <= '0';
-      end if;
-      allvalue(p) <= v(0);
-      accu(p) <= v;
-    end set_accu;
-
-    procedure fixcarry(sign : in std_logic; pos : in position) is
-      variable carrytmp : subblock;
-      variable carrypos : integer;
-      variable i : integer;
+    procedure findcarry(sign : in std_logic; carrypos : inout natural) is
+      variable i : natural;
+      variable start : natural := carrypos + 2;
     begin
       for i in 1 to NUMBLOCKS - 1 loop
-        next when i < pos + 2;
+        next when i < start;
         if allmask(i) = '0' or allvalue(i) = sign then
           carrypos := i;
           exit;
         end if;
         allvalue(i) <= sign;
       end loop;
-      get_accu(carrypos, carrytmp);
+    end;
+
+    procedure fixcarry(sign : in std_logic) is
+    begin
       if sign = '0' then
-        carrytmp := std_logic_vector(unsigned(carrytmp) + 1);
+        curval(BLOCKSIZE-1 downto 0) := std_logic_vector(unsigned(curval(BLOCKSIZE-1 downto 0)) + 1);
       else
-        carrytmp := std_logic_vector(unsigned(carrytmp) - 1);
+        curval(BLOCKSIZE-1 downto 0) := std_logic_vector(unsigned(curval(BLOCKSIZE-1 downto 0)) - 1);
       end if;
-      set_accu(carrypos, carrytmp);
     end fixcarry;
 
-    procedure add(sign : in std_logic; pos : in position; d : in addblock) is
+    procedure add(sign : in std_logic; v : inout addblock) is
       variable result : std_logic_vector(2*BLOCKSIZE downto 0);
-      variable curval : std_logic_vector(2*BLOCKSIZE downto 0);
       variable i : integer;
     begin
-      get_accu(pos, curval(BLOCKSIZE - 1 downto 0));
-      get_accu(pos + 1, curval(2*BLOCKSIZE - 1 downto BLOCKSIZE));
-      curval(2*BLOCKSIZE) := sign;
-      result := std_logic_vector(unsigned(d) + unsigned(curval));
-      set_accu(pos, result(BLOCKSIZE-1 downto 0));
-      set_accu(pos+1, result(2*BLOCKSIZE-1 downto BLOCKSIZE));
-      if result(data'length) = '1' then
-        fixcarry('0', pos);
-      end if;
+      result := std_logic_vector(unsigned(sign&input) + unsigned(v));
+      v := result(input'length-1 downto 0);
+      carry <= result(input'length);
     end add;
   begin
     if reset = '1' then
-      accu <= (others => (others => '0'));
       allmask <= (others => '1');
       allvalue <= (others => '0');
+      carry <= '0';
+      cycle <= '0';
     elsif clock'event and clock = '1' then
-      case op is
-        when op_add =>
-          add('0', pos, data);
-        when op_output =>
-          get_accu(pos, outbuf(BLOCKSIZE-1 downto 0));
-          get_accu(pos+1, outbuf(2*BLOCKSIZE-1 downto BLOCKSIZE));
-          output <= outbuf;
-        when op_nop => null;
-      end case;
+-- copy inputs for use in next cycle
+      addpos := pos;
+      sig_op <= op;
+      input <= data;
+-- start load
+      if allmask(2*addpos0) = '1' then
+        curval0 := (others => allvalue(2*addpos0));
+      else
+        curval0 := accu0(addpos0);
+      end if;
+      if allmask(2*addpos1+1) = '1' then
+        curval1 := (others => allvalue(2*addpos1+1));
+      else
+        curval1 := accu1(addpos1);
+      end if;
+      if swap then
+        curval := curval0 & curval1;
+      else
+        curval := curval1 & curval0;
+      end if;
+-- end load
+      if cycle = '1' then
+        case sig_op is
+          when op_add =>
+            add('0', curval);
+            findcarry('0', addpos);
+          when op_output =>
+            output <= curval;
+          when op_nop => null;
+        end case;
+      elsif carry = '1' then
+        fixcarry('0');
+        carry <= '0';
+      end if;
+ -- start store
+      if swap then
+        curval1 := curval(BLOCKSIZE-1 downto 0);
+        curval0 := curval(2*BLOCKSIZE-1 downto BLOCKSIZE);
+      else
+        curval0 := curval(BLOCKSIZE-1 downto 0);
+        curval1 := curval(2*BLOCKSIZE-1 downto BLOCKSIZE);
+      end if;
+      if curval0 = allone or curval0 = allzero then
+        allmask(2*addpos0) <= '1';
+      end if;
+      allvalue(2*addpos0) <= curval0(0);
+      accu0(addpos0) <= curval0;
+      if curval1 = allone or curval1 = allzero then
+        allmask(2*addpos1+1) <= '1';
+      end if;
+      allvalue(2*addpos1+1) <= curval1(0);
+      accu1(addpos1) <= curval1;
+-- end store
+-- calculate addresses for next read
+      addpos0 <= (addpos + 1) / 2;
+      addpos1 <= addpos / 2;
+      swap <= addpos mod 2 = 1;
+
+      cycle <= not cycle;
     end if;
   end process;
 end behaviour;
