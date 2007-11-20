27b21965f5a1f4973df6bdb29b66857b6556dbe8
Reimar Döffinger <doeffinger@i82pc18.(none)>
2007-11-20 14:01:28 +0100

    Use the right types for data_in and data_out

--- acctop.vhdl
+++ acctop.vhdl
@@ -177,8 +177,8 @@ alias response_cmd_out_unitid : std_logic_vector(5        - 1 downto 0) is respo
 alias response_cmd_out_tag    : std_logic_vector(TAG_LEN  - 1 downto 0) is response_cmd_out(TAG_OFFSET  + TAG_LEN  - 1 downto TAG_OFFSET);
 alias response_cmd_out_format : std_logic_vector(3        - 1 downto 0) is response_cmd_out(95 downto 93);
 
-signal data_in : std_logic_vector(63 downto 0);
-signal data_out : std_logic_vector(63 downto 0);
+signal data_in : addblock;
+signal data_out : addblock;
 signal ready : std_logic;
 signal op : operation;
 signal accreset : std_logic;
