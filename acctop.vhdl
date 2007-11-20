dcd30ef13dad961b08ef3ac54cf0596ab6cd94a2
Reimar Döffinger <reimar@hokum.(none)>
2007-11-20 15:18:07 +0100

    Fix constant declaration and add regnum variable

--- acctop.vhdl
+++ acctop.vhdl
@@ -177,7 +177,7 @@ alias response_cmd_out_unitid : std_logic_vector(5        - 1 downto 0) is respo
 alias response_cmd_out_tag    : std_logic_vector(TAG_LEN  - 1 downto 0) is response_cmd_out(TAG_OFFSET  + TAG_LEN  - 1 downto TAG_OFFSET);
 alias response_cmd_out_format : std_logic_vector(3        - 1 downto 0) is response_cmd_out(95 downto 93);
 
-constant NUMREGS := 8;
+constant NUMREGS : integer := 8;
 type data_array_t is array(0 to NUMREGS-1) of addblock;
 signal data_in : data_array_t;
 signal data_out : data_array_t;
@@ -294,6 +294,7 @@ begin
   clock_output <= clock;
 
   process (clock, reset_n)
+  variable regnum : integer range 0 to NUMREGS-1;
   variable buffered_posted_cmd_avail : std_logic;
   variable buffered_posted_cmd : std_logic_vector(CMD_LEN - 1 downto 0);
   variable buffered_posted_addr : std_logic_vector(ADDR_LEN - 1 downto 0);
