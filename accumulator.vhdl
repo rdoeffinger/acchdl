8112514f5e8285fc51e9395d52a23912691c29c9
Reimar Döffinger <doeffinger@i82pc18.itec.uni-karlsruhe.de>
2008-01-08 16:40:58 +0100

    Rewritten (still incomplete though) accumulator implementation. The Xilinx tool constantly mis-synthesized the previous version, hopefully this one is so simple not even Xilinx tools can break it.

--- accumulator.vhdl
+++ accumulator.vhdl
@@ -48,203 +48,252 @@ entity accumulator is
 end accumulator;
 
 architecture behaviour of accumulator is
-  type state_t is (st_ready, st_add1, st_add2, st_out_block,
-                   st_in_block, st_out_status, st_in_status, st_fixcarry,
-                   st_out_float1, st_out_float2);
+  type state_t is (st_ready, st_add0, st_add1, st_add2, st_fixcarry,
+                   st_out_block0, st_out_block1,
+                   st_in_block, st_out_status, st_in_status,
+                   st_out_float0, st_out_float1, st_out_float2);
 
   signal accu : accutype;
   signal allmask : flagtype;
   signal allvalue : flagtype;
   signal input : addblock;
   signal sig_sign : std_logic;
-  signal addpos : natural range 0 to NUMBLOCKS-1;
+  signal next_pos : natural range 0 to NUMBLOCKS-1;
+  signal read_pos : natural range 0 to NUMBLOCKS-1;
+  signal write_pos : natural range 0 to NUMBLOCKS-1;
+  signal read_block : subblock;
+  signal write_block : subblock;
   signal state : state_t;
   signal out_buf : addblock;
+  signal carry : unsigned(0 downto 0);
   attribute clock_signal : string;
   attribute clock_signal of clock : signal is "yes";
 begin
-  ready <= '1' when state = st_ready and reset = '0' else '0';
+  ready <= '0' when reset = '1' or
+                    state = st_add0 or state = st_add1 or state = st_add2 or
+                    state = st_out_block0 or
+                    state = st_out_float0 or state = st_out_float1
+           else '1';
   data_out <= out_buf;
 
-  process(clock,reset)
-    variable replicate : subblock;
-    variable curval : subblock;
-    variable tmp : addblock;
-    variable carry : std_logic;
-    variable next_addpos : natural range 0 to NUMBLOCKS;
-    variable floatshift : natural range 0 to BLOCKSIZE-1;
+read : process(clock,reset)
+begin
+  if reset = '1' then
+    read_block <= (others => '0');
+  elsif rising_edge(clock) then
+    if next_pos = write_pos then
+      read_block <= write_block;
+    elsif allmask(next_pos) = '1' then
+      read_block <= (others => allvalue(next_pos));
+    else
+      read_block <= accu(next_pos);
+    end if;
+  end if;
+end process;
 
-    procedure findcarry(sign : in std_logic; pos : in position;
-                        carrypos : out natural) is
-      variable i : natural;
-      variable add : natural;
-      variable tmp : flagtype;
-      variable tmp2 : flagtype;
-      variable cptmp : unsigned(BLOCKBITS - 1 downto 0);
-    begin
-      add := 2**pos;
-      if sign = '1' then
-        tmp := allvalue or not allmask;
-        tmp2 := std_logic_vector(unsigned(tmp) - add);
-        tmp := tmp and not tmp2;
-        tmp2 := tmp2 or tmp;
-      else
-        tmp := allvalue and allmask;
-        tmp2 := std_logic_vector(unsigned(tmp) + add);
-        tmp := tmp2 and not tmp;
-        tmp2 := tmp2 and not tmp;
-      end if;
-      if (tmp and X"aaa") /= X"000" then
-        cptmp(0) := '1';
-      else
-        cptmp(0) := '0';
-      end if;
-      if (tmp and X"ccc") /= X"000" then
-        cptmp(1) := '1';
-      else
-        cptmp(1) := '0';
-      end if;
-      if (tmp and X"0f0") /= X"000" then
-        cptmp(2) := '1';
-      else
-        cptmp(2) := '0';
-      end if;
-      if (tmp and X"f00") /= X"000" then
-        cptmp(3) := '1';
-      else
-        cptmp(3) := '0';
-      end if;
-      carrypos := to_integer(cptmp);
-      allvalue <= tmp2;
-    end;
+set_read_pos : process(clock,reset)
+begin
+  if reset = '1' then
+    read_pos <= 0;
+  elsif rising_edge(clock) then
+    read_pos <= next_pos;
+  end if;
+end process;
 
-    procedure fixcarry(sign : in std_logic; v : inout subblock) is
-    begin
-      if sign = '0' then
-        v := std_logic_vector(unsigned(v) + 1);
-      else
-        v := std_logic_vector(unsigned(v) - 1);
-      end if;
-    end fixcarry;
+write : process(clock,reset)
+begin
+  if reset = '1' then
+    null;
+  elsif rising_edge(clock) then
+    accu(write_pos) <= write_block;
+  end if;
+end process;
 
-    procedure add(inc : in subblock; v : inout subblock; carry : inout std_logic) is
-      variable result : std_logic_vector(BLOCKSIZE downto 0);
-      variable c : std_logic_vector(0 downto 0);
-      variable i : integer;
-    begin
-      c(0) := carry;
-      result := std_logic_vector("0"&unsigned(inc) + unsigned(v) + unsigned(c));
-      v := result(BLOCKSIZE-1 downto 0);
-      carry := result(BLOCKSIZE);
-    end add;
-  begin
-    if reset = '1' then
-      allmask <= (others => '1');
-      allvalue <= (others => '0');
-      out_buf <= (others => '0');
-      state <= st_ready;
-    elsif clock'event and clock = '1' then
--- start load
-      if allmask(addpos) = '1' then
-        curval := (others => allvalue(addpos));
-      else
-        curval := accu(addpos);
-      end if;
--- end load
-      case state is
-      when st_out_block =>
+write_allmask : process(clock,reset)
+  variable replicate : subblock;
+begin
+  if reset = '1' then
+    allmask <= (others => '1');
+  elsif rising_edge(clock) then
+    replicate := (others => write_block(0));
+    if write_block = replicate then
+      allmask(write_pos) <= '1';
+    else
+      allmask(write_pos) <= '0';
+    end if;
+	 if state = st_in_status and input(17) = '1' then
+      allmask(NUMBLOCKS) <= not input(1);
+    end if;
+  end if;
+end process;
+
+write_allvalue : process(clock,reset)
+begin
+  if reset = '1' then
+    allvalue <= (others => '0');
+  elsif rising_edge(clock) then
+    allvalue(write_pos) <= write_block(0);
+    if state = st_in_status and input(16) = '1' then
+      allvalue(NUMBLOCKS) <= input(0);
+    end if;
+  end if;
+end process;
+
+execute : process(clock,reset)
+  variable tmp : unsigned(BLOCKSIZE downto 0);
+  variable curval : subblock;
+  variable floatshift : natural range 0 to BLOCKSIZE-1;
+begin
+  if reset = '1' then
+    write_pos <= 0;
+    write_block <= (others => '0');
+  elsif rising_edge(clock) then
+    if read_pos = write_pos then
+      curval := write_block;
+    else
+      curval := read_block;
+    end if;
+    case state is
+      when st_out_block1 =>
         out_buf(BLOCKSIZE-1 downto 0) <= curval;
-        state <= st_ready;
       when st_in_block =>
-        curval := input(BLOCKSIZE-1 downto 0);
-        state <= st_ready;
+        write_pos <= next_pos;
+        write_block <= input(BLOCKSIZE-1 downto 0);
       when st_out_status =>
         out_buf(31 downto 16) <= X"0003"; -- valid flags
         out_buf(15 downto 2) <= (others => '0');
         out_buf(1) <= not allmask(NUMBLOCKS);
         out_buf(0) <= allvalue(NUMBLOCKS);
-        state <= st_ready;
       when st_in_status =>
-        if input(17) = '1' then
-          allmask(NUMBLOCKS) <= not input(1);
-        end if;
-        if input(16) = '1' then
-          allvalue(NUMBLOCKS) <= input(0);
-        end if;
-        state <= st_ready;
+        -- handled in write_allvalue and write_allmask processes
+        null;
       when st_add1 =>
-        carry := '0';
-        add(input(BLOCKSIZE-1 downto 0), curval, carry);
-        addpos <= addpos + 1;
-        state <= st_add2;
+        write_pos <= read_pos;
+        write_pos <= read_pos;
+        tmp := "0"&unsigned(input(BLOCKSIZE-1 downto 0));
+        tmp := tmp + unsigned(curval);
+        carry(0) <= tmp(BLOCKSIZE);
+        write_block <= subblock(tmp(BLOCKSIZE-1 downto 0));
       when st_add2 =>
-        add(input(2*BLOCKSIZE-1 downto BLOCKSIZE), curval, carry);
-        findcarry(sig_sign, addpos + 1, next_addpos);
-        if carry /= sig_sign then
-          state <= st_fixcarry;
-          addpos <= next_addpos;
-        else
-          state <= st_ready;
-          allvalue <= allvalue;
-        end if;
+        write_pos <= read_pos;
+        tmp := "0"&unsigned(input(2*BLOCKSIZE-1 downto BLOCKSIZE));
+        tmp := tmp + unsigned(curval) + carry;
+        carry(0) <= tmp(BLOCKSIZE);
+        write_block <= subblock(tmp(BLOCKSIZE-1 downto 0));
       when st_fixcarry =>
-        fixcarry(sig_sign, curval);
-        state <= st_ready;
+        write_pos <= read_pos;
+        write_block <= subblock(unsigned(curval) + 1);
       when st_out_float1 =>
---        out_buf(BLOCKSIZE-1 downto 0) <= curval;
         for floatshift in BLOCKSIZE-1 downto 0 loop
           if curval(floatshift) /= allvalue(NUMBLOCKS) then
             exit;
           end if;
         end loop;
-        addpos <= addpos - 1;
-        state <= st_out_float2;
       when st_out_float2 =>
         out_buf(31) <= allvalue(NUMBLOCKS);
-        out_buf(30 downto 23) <= std_logic_vector(to_unsigned(addpos * BLOCKSIZE + floatshift - BLOCKSIZE - 1 + 9, 8));
+        out_buf(30 downto 23) <= std_logic_vector(to_unsigned(read_pos * BLOCKSIZE + floatshift - BLOCKSIZE - 1 + 9, 8));
         out_buf(22 downto 0) <= (others => '0');
---        tmp := std_logic_vector(unsigned(std_logic_vector'(out_buf(BLOCKSIZE-1 downto 0) & curval(BLOCKSIZE-1 downto 0))) srl floatshift);
---        out_buf(22 downto 0) <= tmp(31 downto 9);
-        state <= st_ready;
-      when st_ready =>
--- copy inputs for use in next cycles
-        addpos <= pos + NUMBLOCKS / 2;
-        sig_sign <= sign;
-        input <= data_in;
+      when others =>
+        null;
+    end case;
+  end if;
+end process;
+
+get_next_pos : process(clock,reset)
+  variable i : integer;
+begin
+  if reset = '1' then
+    next_pos <= 0;
+  elsif rising_edge(clock) then
+    case state is
+      when st_add0 =>
+        next_pos <= next_pos + 1;
+      when st_add1 =>
+        next_pos <= next_pos + 1;
+      when st_add2 =>
+        next_pos <= 0;
+      when st_out_float0 =>
+        next_pos <= next_pos - 1;
+      when st_out_float1 =>
+        next_pos <= 0;
+      when others =>
         case op is
-        when op_nop => null;
-        when op_add =>
-          if sign = '1' then
-            -- we need two's complement representation
-            input <= addblock(unsigned(not data_in) + 1);
-          end if;
-          state <= st_add1;
-        when op_readblock => state <= st_out_block;
-        when op_writeblock => state <= st_in_block;
-        when op_readflags => state <= st_out_status;
-        when op_writeflags => state <= st_in_status;
-        when op_readfloat =>
-          addpos <= 0;
-          for next_addpos in NUMBLOCKS - 1 downto 0 loop
-            if allmask(next_addpos) = '0' or
-               allvalue(next_addpos) /= allvalue(NUMBLOCKS) then
-              addpos <= next_addpos;
-              exit;
-            end if;
-          end loop;
-          state <= st_out_float1;
+          when op_add | op_readblock | op_writeblock =>
+            next_pos <= pos + NUMBLOCKS / 2;
+          when op_readfloat =>
+-- FIXME this is wrong here
+            next_pos <= 0;
+            for i in NUMBLOCKS - 1 downto 0 loop
+              if allmask(i) = '0' or
+                 allvalue(i) /= allvalue(NUMBLOCKS) then
+                next_pos <= i;
+                exit;
+              end if;
+            end loop;
+          when others =>
+            next_pos <= 0;
         end case;
-      end case;
- -- start store
-      replicate := (others => curval(0));
-      if curval = replicate then
-        allmask(addpos) <= '1';
-      else
-        allmask(addpos) <= '0';
-      end if;
-      allvalue(addpos) <= curval(0);
-      accu(addpos) <= curval;
--- end store
-    end if;
-  end process;
+    end case;
+  end if;
+end process;
+
+state_handling : process(clock,reset)
+begin
+  if reset = '1' then
+    state <= st_ready;
+  elsif rising_edge(clock) then
+    case state is
+      when st_add0 =>
+        state <= st_add1;
+      when st_add1 =>
+        state <= st_add2;
+      when st_add2 =>
+        state <= st_fixcarry;
+      when st_out_float0 =>
+        state <= st_out_float1;
+      when st_out_float1 =>
+        state <= st_out_float2;
+      when st_out_block0 =>
+        state <= st_out_block1;
+      when others =>
+        case op is
+          when op_nop =>
+            state <= st_ready;
+          when op_add =>
+            state <= st_add0;
+          when op_readblock =>
+            state <= st_out_block0;
+          when op_writeblock =>
+            state <= st_in_block;
+          when op_readflags =>
+            state <= st_out_status;
+          when op_writeflags =>
+            state <= st_in_status;
+          when op_readfloat =>
+            state <= st_out_float0;
+        end case;
+    end case;
+  end if;
+end process;
+
+get_input : process(clock,reset)
+begin
+  if reset = '1' then
+    input <= (others => '0');
+    sig_sign <= '0';
+  elsif rising_edge(clock) then
+    case state is
+      when st_add1 | st_add2 | st_out_float1 =>
+        null;
+      when others =>
+        if op = op_add and sign = '1' then
+          input <= addblock(unsigned(not data_in) + 1);
+        else
+          input <= data_in;
+        end if;
+        sig_sign <= sign;
+    end case;
+  end if;
+end process;
+
 end behaviour;
