88ba97305a2ca61076b56ddafe3f3adfae7f640b
Reimar Döffinger <doeffinger@i82pc18.itec.uni-karlsruhe.de>
2008-01-22 10:57:26 +0100

    Add negative float test-case

--- test_accumulator.vhdl
+++ test_accumulator.vhdl
@@ -16,18 +16,17 @@ architecture behaviour of test_accumulator is
   signal acc_res : subblock;
   signal testcycle : integer := 0;
 
-  constant NUMTESTS : integer := 9;
+  constant NUMTESTS : integer := 10;
   type ops_t is array (0 to NUMTESTS - 1) of operation;
   constant ops : ops_t := (
-    op_nop, op_floatadd, op_floatadd, op_readfloat,
+    op_nop, op_floatadd, op_floatadd, op_floatadd, op_readfloat,
     op_add, op_add, op_add, op_readblock, op_readfloat
   );
 
   type datas_t is array (0 to NUMTESTS - 1) of addblock;
   constant datas : datas_t := (
-    (others => 'Z'), X"000000003f800000", X"0000000040000000", (others => '1'),
-    X"0123456789abcdef", X"19acdefffffff000", (others => '1'),
-    (others => 'Z'), (others => 'Z')
+    (others => 'Z'), X"000000003f800000", X"0000000040000000", X"00000000c0a00000", (others => '1'),
+    X"0123456789abcdef", X"19acdefffffff000", (others => '1'), (others => 'Z'), (others => 'Z')
   );
 
   constant pos0 : position_t := "000000000";
@@ -36,13 +35,13 @@ architecture behaviour of test_accumulator is
 
   type poss_t is array (0 to NUMTESTS - 1) of position_t;
   constant poss : poss_t := (
-    pos0, pos0, pos0, pos0,
+    pos0, pos0, pos0, pos0, pos0,
     pos1, pos1, pos1, pos3, pos0
   );
 
   type resets_t is array (0 to NUMTESTS - 1) of std_logic;
   constant resets : resets_t := (
-    '1', '0', '0', '0', '0', '0', '0', '0', '0'
+    '1', '0', '0', '0', '0', '0', '0', '0', '0', '0'
   );
 
 constant ACC_CLOCK_PERIOD : time := 10ns;
