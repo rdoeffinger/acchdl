594fc43465603c7c6d2ee05792299b6b3360127c
Reimar Döffinger <reimar@hokum.(none)>
2008-01-31 12:28:20 +0100

    Keep upper half of the register command area as reserved

--- ht_mmap_if.vhdl
+++ ht_mmap_if.vhdl
@@ -220,7 +220,7 @@ begin
       if ready(cmd_reg) = '1' then
         data_in(cmd_reg) <= data;
         pos(cmd_reg) <= not addr(8) & addr(7 downto 0);
-        sign(cmd_reg) <= addr(8);
+        sign(cmd_reg) <= addr(7);
       end if;
     end if;
   end process;
