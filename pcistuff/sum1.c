f4ce1985c63e81283cea5f3c8c1914c63fef4cb4
Reimar Döffinger <doeffinger@i82pc18.itec.uni-karlsruhe.de>
2008-02-06 16:12:04 +0100

    Summation test program

---
+++ pcistuff/sum1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "libefac.h"
+int main(void) {
+  int i;
+  double v1 = 0;
+  float v2 = 0;
+  if (!efac_init()) {
+    printf("init failed!\n");
+    return 1;
+  }
+  efac_clear(0);
+  for (i = 1; i < 1000000; i++) {
+    v1 += 1.0 / i;
+    v2 += 1.0 / i;
+    efac_add(0, 1.0 / i);
+  printf("vals: %f %f %f\n", v1, v2, efac_read(0));
+  }
+  printf("vals: %f %f %f\n", v1, v2, efac_read(0));
+  return 0;
+}
