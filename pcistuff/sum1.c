5b19344b255c44e70edfea4376f703adc2ca8b8c
Reimar Döffinger <doeffinger@i82pc18.itec.uni-karlsruhe.de>
2008-02-12 11:05:05 +0100

    Change summation test program

--- pcistuff/sum1.c
+++ pcistuff/sum1.c
@@ -9,12 +9,12 @@ int main(void) {
     return 1;
   }
   efac_clear(0);
-  for (i = 1; i < 1000000; i++) {
+  for (i = 1; i < 100000000; i++) {
     v1 += 1.0 / i;
     v2 += 1.0 / i;
     efac_add(0, 1.0 / i);
-  printf("vals: %f %f %f\n", v1, v2, efac_read(0));
+//  printf("vals: %f %f %f\n", v1, v2, efac_read(0));
   }
-  printf("vals: %f %f %f\n", v1, v2, efac_read(0));
+  printf("vals: %e %e %e\n", v1, v2, efac_read(0));
   return 0;
 }
