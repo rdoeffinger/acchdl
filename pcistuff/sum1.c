3b1b5e886049fd0ba16f0b775ca856da7dc31e39
Reimar Döffinger <doeffinger@i82lfs02.irf.uni-karlsruhe.de>
2008-02-14 17:02:54 +0100

    Variable number of adds per loop iteration

--- pcistuff/sum1.c
+++ pcistuff/sum1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "libefac.h"
+#define PER_LOOP 8
 int main(void) {
   int i;
   double v1 = 0;
@@ -9,10 +10,19 @@ int main(void) {
     return 1;
   }
   efac_clear(0);
-  for (i = 1; i < 100000000; i++) {
-    v1 += 1.0 / i;
-    v2 += 1.0 / i;
-    efac_add(0, 1.0 / i);
+  for (i = 1; i < 100000000; i+=PER_LOOP) {
+//    v1 += 1.0 / i;
+//    v2 += 1.0 / i;
+#if PER_LOOP == 1
+    efac_add(0, 1.0 / (i + 0));
+#elif PER_LOOP == 4
+    efac_add4(0, 1.0 / (i + 0), 1.0 / (i + 1), 1.0 / (i + 2), 1.0 / (i + 3));
+#elif PER_LOOP == 8
+    efac_add8(0, 1.0 / (i + 0), 1.0 / (i + 1), 1.0 / (i + 2), 1.0 / (i + 3),
+                 1.0 / (i + 4), 1.0 / (i + 5), 1.0 / (i + 6), 1.0 / (i + 7));
+#else
+#error unsuppoted PER_LOOP value
+#endif
 //  printf("vals: %f %f %f\n", v1, v2, efac_read(0));
   }
   printf("vals: %e %e %e\n", v1, v2, efac_read(0));
