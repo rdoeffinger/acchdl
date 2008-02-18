aab2785a2e528108f3c68aa8abcea454d33f9a94
Reimar Döffinger <doeffinger@i82pc18.itec.uni-karlsruhe.de>
2008-02-18 14:59:36 +0100

    Macro for writes

--- pcistuff/libefac.h
+++ pcistuff/libefac.h
@@ -10,6 +10,12 @@
 #define EFAC_BARRIER(var) asm("clflush %0\n\t"::"m"(var):"memory")
 #endif
 
+#if 1
+#define EFAC_WRITE(var, val) (var) = (val)
+#else
+#define EFAC_WRITE(var, val) asm("movnti %1, %0\n\t" : "=m"(var) : "r"(val))
+#endif
+
 extern volatile uint8_t efac_regs[];
 extern int efac_idx;
 int efac_init(void);
@@ -18,13 +24,13 @@ void efac_restore(int reg, const uint32_t buf[512]);
 
 static inline efac_unused void efac_clear(int reg) {
   volatile uint32_t *regb = (volatile uint32_t *)&efac_regs[reg * 4096];
-  regb[512] = 0x00070004;
+  EFAC_WRITE(regb[512], 0x00070004);
   EFAC_BARRIER(regb[512]);
 }
 
 static inline efac_unused void efac_add(int reg, float val) {
   volatile float *regb = (volatile float *)&efac_regs[reg * 4096];
-  regb[efac_idx++] = val;
+  EFAC_WRITE(regb[efac_idx++], val);
   efac_idx &= 7;
   if (efac_idx)
     return;
@@ -33,7 +39,7 @@ static inline efac_unused void efac_add(int reg, float val) {
 
 static inline efac_unused void efac_sub(int reg, float val) {
   volatile float *regb = (volatile float *)&efac_regs[reg * 4096];
-  regb[128 + efac_idx++] = val;
+  EFAC_WRITE(regb[128 + efac_idx++], val);
   efac_idx &= 7;
   if (efac_idx)
     return;
@@ -43,10 +49,10 @@ static inline efac_unused void efac_sub(int reg, float val) {
 static inline efac_unused void efac_add4(int reg,
           float val1, float val2, float val3, float val4) {
   volatile float *regb = (volatile float *)&efac_regs[reg * 4096];
-  regb[16+0] = val1;
-  regb[16+1] = val2;
-  regb[16+2] = val3;
-  regb[16+3] = val4;
+  EFAC_WRITE(regb[16+0], val1);
+  EFAC_WRITE(regb[16+1], val2);
+  EFAC_WRITE(regb[16+2], val3);
+  EFAC_WRITE(regb[16+3], val4);
   EFAC_BARRIER(regb[16]);
 }
 
